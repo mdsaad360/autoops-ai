d1a151ca752f84f0afd6a47c55c8bbc9376a9248
mdsaad360 <ansarisaad944@gmail.com>
2025-09-26 05:40:50 +0530

    Phase 5b: Replace dummy predict with hugging face Replace dummy predict endpoint with hugging face sentiment model

--- app/main.py
+++ app/main.py
@@ -1,11 +1,16 @@
 from fastapi import FastAPI
 from pydantic import BaseModel
+from transformers import pipeline
 import logging
 
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
 
 app = FastAPI()
+#Load sentiment analysis model at startup
+#sentiment_pipeline = pipeline("sentiment-analysis")
+
+sentiment_pipeline = pipeline("sentiment-analysis", model="distilbert-base-uncased-finetuned-sst-2-english", revision="714eb0f")
 
 @app.get("/health")
 def health_check():
@@ -22,13 +27,30 @@ class PredictRequest(BaseModel):
     text: str
 
 #Dummy predict endpoint
+# @app.post("/predict")
+# def predict(request: PredictRequest):
+#     logger.info("Predict endpoint called")
+#     #Dummy logic: classsify text length
+#     if len(request.text) % 2 == 0:
+#         sentiment = "positive"
+#     else:
+#         sentiment = "negative"
+#     logger.info(f"Predicted sentiment: {sentiment}")
+#     return {"input": request.text, "sentiment": sentiment}
+
 @app.post("/predict")
 def predict(request: PredictRequest):
     logger.info("Predict endpoint called")
-    #Dummy logic: classsify text length
-    if len(request.text) % 2 == 0:
-        sentiment = "positive"
-    else:
-        sentiment = "negative"
-    logger.info(f"Predicted sentiment: {sentiment}")
-    return {"input": request.text, "sentiment": sentiment}
+    try:
+        #results = sentiment_pipeline(request.text)
+        #sentiment = results[0]['label'].lower()
+        results = sentiment_pipeline(request.text)[0]
+        logger.info(f"Predicted sentiment: {results['label']} with score {results['score']  }")
+        return {
+            "input": request.text,
+            "label": results['label'],
+            "score": results['score']
+        }
+    except Exception as e:
+        logger.error(f"Error during prediction: {e}")
+        return {"error": "Prediction failed"}
